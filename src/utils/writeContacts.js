46f8645b76b88fdf893997d73dbc2f1bc43d4372
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-09-04 00:33:08 +0300

    chanched writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -6,6 +6,6 @@ import fs from 'node:fs/promises';
 export const writeContacts = async (updatedContacts) => {
     const data = await readContacts();
     const transformData = [...data, ...updatedContacts];
-    await fs.writeFile(PATH_DB, JSON.stringify(transformData), { encoding: 'utf-8' });
+    await fs.writeFile(PATH_DB, JSON.stringify(transformData, null, 2), { encoding: 'utf-8' });
 };
 writeContacts().catch(error => console.error(error));
