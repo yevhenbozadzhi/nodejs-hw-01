3a80f420dda958d53b8b01ae8975aa040b2453c8
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-09-05 17:06:42 +0300

    done

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,10 +1,13 @@
 import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
 import { writeContacts } from "../utils/writeContacts.js";
 
 export const addOneContact = async () => {
     try {
+        const oldContacts = await readContacts();
         const oneContact = createFakeContact();
-        await writeContacts(oneContact);
+        const addOneContact = [...oldContacts, oneContact];
+        await writeContacts(addOneContact);
         console.log("new one contact added", oneContact);
 
   }
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,11 +1,14 @@
 import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 
 const generateContacts = async (number) => {
     try {
-    const newContacts = Array.from({ length: number }, () => createFakeContact());
-        await writeContacts(newContacts);
+        const oldContacts = await readContacts();
+        const newContacts = Array.from({ length: number }, () => createFakeContact());
+        const allContacts = [...oldContacts, ...newContacts];
+        await writeContacts(allContacts);
         console.log(`${number}, done!`);
     }
     catch (error) {
